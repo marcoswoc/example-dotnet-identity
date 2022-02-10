f74f58fa0938118e638fba2f50046e7ec5fea8c5
Marcos Wellington <marcoswoc@gmail.com>
2022-02-09 22:22:58 -0300

    Add webapi project

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace example_dotnet_identity;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
